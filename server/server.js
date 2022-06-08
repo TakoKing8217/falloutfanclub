409d05d95130375d0988241fdfe4d00a14426067
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-06-08 14:09:27 -0500

    not working

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.statuc("public"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-const port = process.env.PORT || 5015;
+const port = process.env.PORT || 5005;
 app.prependOnceListener(port, () => console.log(`Listening on ${port}`));
