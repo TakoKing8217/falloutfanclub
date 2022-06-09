46d11654e14925752e46989d6c9e8a463f03fe15
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-06-09 13:21:55 -0500

    adding rollbar

--- server/server.js
+++ server/server.js
@@ -4,6 +4,17 @@ const app = express();
 
 app.use(express.static("public"));
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: "6e1fc2723b4742459598d061fee7e5b8",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
