8bd16b2fd9a299e4c7a487a345a9a5214282c432
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-06-09 15:31:50 -0500

    did some stuff

--- public/main.js
+++ public/main.js
@@ -1,3 +1,16 @@
+const BASE_URL = "http://localhost:5500";
+
 const chooseBtn = document.querySelector("button");
+const hoursBtn = document.querySelector("#hours");
+
+const randomHours = (event) => {
+  console.log("hit1");
+  event.preventDefault;
+  axios
+    .get(`${BASE_URL}/hours`)
+    .then((res) => alert(res.data))
+    .catch((err) => console.log(err));
+};
 
 chooseBtn.addEventListener("click", () => alert("Fallout 3."));
+hoursBtn.addEventListener("click", randomHours);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
-app.use(express.static("public"));
+const cors = require("cors");
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require("rollbar");
@@ -15,9 +14,19 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log("Hello world!");
 
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-const port = process.env.PORT || 5005;
+app.get("/hours", (req, res) => {
+  rollbar.info("Someone asked how long they should play.");
+  let hours = "4 hours";
+  res.status(200).send(hours);
+});
+
+const port = process.env.PORT || 5500;
 app.listen(port, () => console.log(`Listening on ${port}`));
